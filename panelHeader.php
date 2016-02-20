9c68430ec9929973f9953453020a3329e6a63657
marekdan <marekdan4@gmail.com>
2016-02-20 14:55:05 +0100

    Modified panel and added header to each panel

---
+++ panelHeader.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8">
+    <title>
+        Panel Zarządzania
+    </title>
+</head>
+<body>
+<div>
+    <a href="panelMain.php">
+        <div>
+            Panel główny
+        </div>
+    </a>
+    <a href="panelProduct.php">
+        <div>
+            Zarządzanie przedmiotami
+        </div>
+    </a>
+
+    <a href="panelUser.php">
+        <div>
+            Zarządzanie użytkownikami
+        </div>
+    </a>
+    <a href="panelOrder.php">
+        <div>
+            Zarządzanie zamównieniami
+        </div>
+    </a>
+    <hr>
+</div>
+</body>
+</html>
--- src/Admin.php
+++ src/Admin.php
@@ -11,12 +11,62 @@ CREATE TABLE Admin(
 
 */
 
-class Admin{
+class Admin {
+
+    private $id;
+    private $name;
+    private $email;
+    private $password;
 
     static private $connection;
 
     static public function SetConnection(mysqli $newConnection) {
         Admin::$connection = $newConnection;
+    }
+
+    static public function RegisterAdmin($newEmail, $password1, $password2){
+
+        if($password1 !== $password2){
+            return false;
+        }
+
+        $options = [
+            'cost'=>11,
+            'salt'=>mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+        ];
+        $hashedPassword = password_hash($password1, PASSWORD_BCRYPT, $options);
+
+        $sql = "INSERT INTO Users(email, password) VALUES('$newEmail', '$hashedPassword')";
+
+        $result = self::$connection->query($sql);
+        if($result === TRUE) {
+            $newUser = new User(self::$connection->insert_id, $newEmail);
+            return $newUser;
+        }
+        return false;
+
+    }
+
+    static public function LogInAdmin($email, $password) {
+        $sql = "SELECT * FROM Users WHERE email like '$email'";
+        $result = self::$connection->query($sql);
+
+        if ($result !== false) {
+            if ($result->num_rows === 1) {
+                $row = $result->fetch_assoc();
+
+                $isPasswordOk = password_verify($password, $row["password"]);
+
+                if ($isPasswordOk === true) {
+                    $user = new User($row["id"], $row["name"], $row["email"], $row["streetNumber"], $row["houseNumber"],
+                        $row["postCode"], $row["city"]);
+
+                    return $user;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
